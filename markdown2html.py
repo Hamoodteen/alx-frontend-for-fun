3032f9875526b19facbbc54d95147be673aec3cb
Hamoodteen <ahmed104567@outlook.com>
2024-05-09 22:59:40 +0300

    0

--- markdown2html.py
+++ markdown2html.py
@@ -5,10 +5,10 @@ import os
 
 
 if len(sys.argv) < 2:
-    sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
+    sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
     sys.exit(1)
-elif not os.path.isfile(f"./{sys.argv[1]}"):
-    sys.stderr.write(f"Missing {sys.argv[1]}")
+elif not os.path.isfile(f"./{sys.argv[2]}"):
+    sys.stderr.write(f"Missing {sys.argv[2]}\n")
     sys.exit(1)
 else:
     sys.exit(0)
