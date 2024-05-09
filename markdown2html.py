659ecb8bc35bfcdd008be6cfcc2801af458d3abe
Hamoodteen <ahmed104567@outlook.com>
2024-05-09 23:00:37 +0300

    0

--- markdown2html.py
+++ markdown2html.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         sys.exit(1)
-    elif not os.path.isfile(f"./{sys.argv[2]}"):
-        sys.stderr.write(f"Missing {sys.argv[2]}\n")
+    elif not os.path.isfile(f"./{sys.argv[1]}"):
+        sys.stderr.write(f"Missing {sys.argv[1]}\n")
         sys.exit(1)
     else:
         sys.exit(0)
