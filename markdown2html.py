d876f6ce055aa4629f07ec54110b512991d1846b
Hamoodteen <ahmed104567@outlook.com>
2024-05-09 23:14:34 +0300

    0

--- markdown2html.py
+++ markdown2html.py
@@ -12,4 +12,17 @@ if __name__ == "__main__":
         sys.stderr.write(f"Missing {sys.argv[1]}\n")
         sys.exit(1)
     else:
-        sys.exit(0)
+        with open(f"./{sys.argv[1]}", "r") as readme, open(f"./{sys.argv[2]}", "a") as html:
+            for line in readme.readlines():
+                if line.startswith("# "):
+                    html.write(f"<h1>{line}</h1>")
+                elif line.startswith("## "):
+                    html.write(f"<h2>{line}</h2>")
+                elif line.startswith("### "):
+                    html.write(f"<h3>{line}</h3>")
+                elif line.startswith("#### "):
+                    html.write(f"<h4>{line}</h4>")
+                elif line.startswith("##### "):
+                    html.write(f"<h5>{line}</h5>")
+                elif line.startswith("###### "):
+                    html.write(f"<h6>{line}</h6>")
