f3361d5fc9c39464c05a5385c3599ad0e8d4d5ee
Hamoodteen <ahmed104567@outlook.com>
2024-05-09 22:58:23 +0300

    0

--- markdown2html.py
+++ markdown2html.py
@@ -7,8 +7,8 @@ import os
 if len(sys.argv) < 2:
     sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
     sys.exit(1)
-elif not os.path.exists(sys.argv[1]):
-    sys.stderr.write(f"Missing <filename>")
+elif not os.path.isfile(f"./{sys.argv[1]}"):
+    sys.stderr.write(f"Missing {sys.argv[1]}")
     sys.exit(1)
 else:
     sys.exit(0)
